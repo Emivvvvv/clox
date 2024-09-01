dea2161b1da2f59e900ad5b797e663b1c5b512df
Emirhan TALA <tala.emirhan@gmail.com>
2024-09-01 15:15:36 +0300

    implemented chapter 15, A Virtual Machine

--- main.c
+++ main.c
@@ -5,19 +5,34 @@
 #include "common.h"
 #include "chunk.h"
 #include "debug.h"
+#include "vm.h"
 
 int main(int argc, const char* argv[]) {
+    init_vm();
+
     Chunk chunk;
     init_chunk(&chunk);
 
     int constant = add_constant(&chunk, 1.2);
     write_chunk(&chunk, OP_CONSTANT, 123);
     write_chunk(&chunk, constant, 123);
+    constant = add_constant(&chunk, 3.4);
+    write_chunk(&chunk, OP_CONSTANT, 123);
+    write_chunk(&chunk, constant, 123);
+
+    write_chunk(&chunk, OP_ADD, 123);
+
+    constant = add_constant(&chunk, 5.6);
+    write_chunk(&chunk, OP_CONSTANT, 123);
+    write_chunk(&chunk, constant, 123);
+
+    write_chunk(&chunk, OP_DIVIDE, 123);
+    write_chunk(&chunk, OP_NEGATE, 123);
 
     write_chunk(&chunk, OP_RETURN, 123);
 
-    disassemble_chunk(&chunk, "test chunk");
-
+    interpret(&chunk);
+    free_vm();
     free_chunk(&chunk);
 
     return 0;
---
+++ vm.h
@@ -0,0 +1,33 @@
+//
+// Created by Emirhan Tala on 1.09.2024.
+//
+
+#ifndef CLOX_VM_H
+#define CLOX_VM_H
+
+#include "chunk.h"
+#include "value.h"
+
+#define STACK_MAX 256
+
+typedef struct {
+    Chunk* chunk;
+    uint8_t* ip;
+    Value stack[STACK_MAX];
+    Value* stack_top;
+} VM;
+
+typedef enum {
+    INTERPRET_OK,
+    INTERPRET_COMPILE_ERROR,
+    INTERPRET_RUNTIME_ERROR
+} InterpretResult;
+
+void init_vm();
+void free_vm();
+InterpretResult interpret(Chunk* chunk);
+void push(Value value);
+Value pop();
+
+
+#endif //CLOX_VM_H
